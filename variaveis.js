b567262f979fb7778503a296ecac53669ad1dd93
Gabriela Saqui <122561794+gabrielasaqui@users.noreply.github.com>
2023-06-27 21:52:27 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+// Declaração das variáveis e atribuição de valores
+var numero1 = 10;
+var numero2 = 5;
+
+// Soma
+var soma = numero1 + numero2;
+console.log("Soma:", soma);
+
+// Divisão
+var divisao = numero1 / numero2;
+console.log("Divisão:", divisao);
+
+// Multiplicação
+var multiplicacao = numero1 * numero2;
+console.log("Multiplicação:", multiplicacao);
+
+// Comparação para verificar se os valores são iguais
+var saoIguais = numero1 === numero2;
+console.log("Os valores são iguais?", saoIguais);
